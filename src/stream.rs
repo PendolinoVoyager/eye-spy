46ca2cc50106d5ac115d1e158d3aeb87d8dbfd79
KP <q6894kjz8x34@gmail.com>
2024-09-26 20:52:37 +0200

    making ffi for decoder

--- src/stream.rs
+++ src/stream.rs
@@ -2,8 +2,10 @@
 //! It provides basic frame encoding/decoding functionalities
 
 use lazy_static::lazy_static;
+use openh264::decoder::Decoder;
 use openh264::encoder::{EncodedBitStream, Encoder};
 use openh264::formats::YUVSlices;
+use openh264::nal_units;
 use std::io::BufWriter;
 use std::net::UdpSocket;
 use std::sync::Mutex;
@@ -29,76 +31,78 @@ const PACKET_DATA_SIZE: u32 = 504;
 
 // Static buffers so the borrow checker doesn't complain
 lazy_static! {
-    pub static ref YUV_FRAME_BUFFER_WRITER: Mutex<Vec<u8>> =
-        Mutex::new(Vec::with_capacity(WIDTH * HEIGHT * 2));
+    pub static ref YUV_FRAME_BUFFER: Mutex<Vec<u8>> =
+        Mutex::new(Vec::with_capacity(WIDTH * HEIGHT * 2 * 30));
 }
-/// YUYV data frame
-pub struct FrameBuilder {
+
+/// NAL unit builder for a H.264 stream over UDP.
+/// The NAL units cannot be safely sent over UDP without splitting them into smaller packets.
+/// This object uses the underlying decoder only when the whole NAL unit can be re-created.
+/// TODO: UDP PACKETS MIGHT COME UNORDERED, SAVE THE LAST N OF THEM AND LOOK IF CAN RECOVER
+pub struct NalBuilder {
     pub finished: bool,
-    /// The buffer for the frame. To allow mutable access while copying, there are two
-    /// Need to be on the heap, stack overflow otherwise
-    buffers: [Box<[u8; 1024 * 512]>; 2],
-    /// Index of current buffer to write to
-    selected_buffer: usize,
-    /// Missing data in the frame b/// Lastuffer
+    pub failed: bool,
+    /// The buffer for the nal unit. For safety purposes, it's set to the max NAL unit size possible
+    nal_unit_buffer: Box<[u8; 65535]>,
+    /// Identifier of the last packet. If the packet is lost, the NAL unit build is failed
     last_packet: PacketIdentifier,
+    end_idx: usize,
 }
-impl Default for FrameBuilder {
+impl Default for NalBuilder {
     fn default() -> Self {
         Self {
             finished: false,
-            buffers: [Box::new([0; 1024 * 512]), Box::new([0; 1024 * 512])],
+            failed: false,
+            nal_unit_buffer: Box::new([0; 65535]),
             last_packet: 0,
-            selected_buffer: 0,
+            end_idx: 0,
         }
     }
 }
-impl FrameBuilder {
+impl NalBuilder {
     pub fn new() -> Self {
         Self::default()
     }
-    fn switch_buffer(&mut self) {
-        self.last_packet = 0;
-        self.selected_buffer = self.next_buffer();
-        self.finished = false;
-        let time = std::time::Instant::now();
-
-        dbg!(time, "Frame finished.");
-    }
-
-    pub fn get_last_frame(&self) -> Option<&[u8; 1024 * 512]> {
-        let other_buffer = (self.selected_buffer + 1) % self.buffers.len();
-        Some(&self.buffers[other_buffer])
-    }
-
-    /// Add data from the buffer. The more, the better
-    pub fn add_data(&mut self, buf: &[u8], n: usize) {
-        if buf.starts_with(FRAME_END) && n == 11 {
-            self.finished = true;
-        } else if let Ok((data, ident)) = Self::decode_frame(&buf[0..n]) {
-            // Clear if START frame was missed
-            if self.finished || ident <= self.last_packet {
-                self.switch_buffer();
-            }
-            let missing_packets = ident - 1 - self.last_packet;
-
-            let offset = missing_packets * PACKET_DATA_SIZE;
-
-            for (idx, byte) in data.iter().enumerate() {
-                self.buffers[self.selected_buffer]
-                    [((self.last_packet * PACKET_DATA_SIZE + offset) + idx as u32) as usize] =
-                    *byte;
-            }
-
-            // If packets are missing
-
-            self.last_packet = ident;
+    pub fn get_nal_unit(&self) -> Option<&[u8]> {
+        if self.finished && !self.failed {
+            Some(&self.nal_unit_buffer[0..self.end_idx])
+        } else {
+            None
         }
     }
-    /// Gets the next buffer that's supposed to be used, doesn't mutate the current state
-    fn next_buffer(&self) -> usize {
-        (self.selected_buffer + 1) % self.buffers.len()
+
+    fn reset(&mut self) {
+        self.finished = false;
+        self.failed = false;
+        self.last_packet = 0;
+        self.end_idx = 0;
     }
+    /// Add data from the buffer. The more, the better
+    pub fn add_data(&mut self, buf: &[u8]) {
+        if buf.starts_with(FRAME_END) && buf.len() == 11 {
+            self.finished = true;
+        } else if let Ok((data, ident)) = Self::decode_frame(buf) {
+            if self.finished || ident <= self.last_packet {
+                self.reset();
+            }
+            if self.failed {
+                return;
+            }
+            let missing_packets = ident - 1 - self.last_packet;
+            if missing_packets > 0 {
+                self.failed = true;
+                return;
+            };
+            self.last_packet = ident;
+            // Copy the data into the buffer at correct slot
+            for (idx, byte) in data.iter().enumerate() {
+                self.nal_unit_buffer
+                    [((self.last_packet * PACKET_DATA_SIZE) + idx as u32) as usize] = *byte;
+            }
+            self.end_idx += data.len();
+        }
+    }
+
     /// Decodes frame. Returns data and identifier
     /// Returned error doesn't matter, we can lose the packet
     fn decode_frame(data: &[u8]) -> Result<(&[u8], u32), ()> {
@@ -159,17 +163,14 @@ impl<'a> H264Stream<'a> {
 
     fn get_encoded_stream(&mut self) -> Result<EncodedBitStream, String> {
         const STRIDES: (usize, usize, usize) = (WIDTH, WIDTH, WIDTH);
+        let buffer = self.stream.next().map_err(|e| e.to_string())?.0;
 
-        let raw_buf = {
-            let (raw_buf, _) = self.stream.next().map_err(|e| e.to_string())?;
-            raw_buf
-        };
-
-        let slices = Self::prepare_yuv_slices(raw_buf, WIDTH, HEIGHT);
+        let slices = Self::prepare_yuv_slices(buffer, WIDTH, HEIGHT);
 
         let slices = YUVSlices::new((&slices.0, &slices.1, &slices.2), (WIDTH, HEIGHT), STRIDES);
 
         let encoded = self.encoder.encode(&slices).map_err(|e| e.to_string())?;
+
         Ok(encoded)
     }
 }
@@ -197,6 +198,7 @@ impl CustomStream<'_, MmapStream<'_>> for H264Stream<'_> {
     fn next_vec(&mut self) -> Option<Vec<u8>> {
         if let Ok(bitstream) = self.get_encoded_stream() {
             let mut vec = Vec::new();
+
             if bitstream.write(&mut vec).is_err() {
                 return None;
             }
@@ -220,12 +222,25 @@ pub(crate) fn init_client_streams() {
         let udp_receiver = UdpSocket::bind("127.0.0.1:7000").unwrap();
         udp_receiver.connect("127.0.0.1:6969").unwrap();
         let mut recv_buf: [u8; 1024] = [0; 1024];
-        let mut frame = FrameBuilder::default();
         // Don't do more than 60 fps
+        let mut nal_builder = NalBuilder::new();
+        let mut decoder = Decoder::new().unwrap();
         loop {
             while let Ok(bytes_read) = udp_receiver.recv(&mut recv_buf) {
-                frame.add_data(&recv_buf, bytes_read);
-                if frame.finished {}
+                nal_builder.add_data(&recv_buf[0..bytes_read]);
+                if nal_builder.finished && !nal_builder.failed {
+                    let unit = nal_builder.get_nal_unit();
+                    if unit.is_none() {
+                        continue;
+                    }
+                    dbg!("Got a nal unit!");
+
+                    match decoder.decode(unit.unwrap()) {
+                        Err(e) => (),
+                        Ok(Some(d)) => println!("GOT FRAME!!!!"),
+                        Ok(None) => println!("No frame..."),
+                    }
+                }
             }
         }
     });
@@ -233,27 +248,24 @@ pub(crate) fn init_client_streams() {
     spawn(move || {
         let udp_transmitter = UdpSocket::bind("127.0.0.1:6969").unwrap();
         udp_transmitter.connect("127.0.0.1:7000").unwrap();
-        // max safe udp packet is 508 bytes!!!
-        // so: send 504 bytes and one int identifier
-        let mut buf = Vec::with_capacity(WIDTH * HEIGHT * 2);
+        // Sending NAL packets, which can be lost
+        // let mut buf = Vec::with_capacity(WIDTH * HEIGHT * 2);
         loop {
-            let len = stream.next(&mut buf);
+            let buf = stream.next_vec();
 
-            if len.is_none() {
+            if buf.is_none() {
                 continue;
             }
-
-            for (num, packet) in buf[0..len.unwrap()]
-                .chunks(PACKET_DATA_SIZE as usize)
-                .enumerate()
-            {
-                let mut packet_with_ident = Vec::with_capacity(PACKET_DATA_SIZE as usize + 4); // Allocate enough space
-                packet_with_ident.extend_from_slice(packet); // Append the packet data
-                let num_as_bytes = (num as u32 + 1).to_le_bytes(); // Convert num (usize) to 4 bytes (u32)
-                packet_with_ident.extend_from_slice(&num_as_bytes); // Append the identifier
-                udp_transmitter.send(&packet_with_ident).unwrap();
+            for packet in nal_units(&buf.unwrap()) {
+                for (num, packet) in packet.chunks(PACKET_DATA_SIZE as usize).enumerate() {
+                    let mut packet_with_ident = Vec::with_capacity(PACKET_DATA_SIZE as usize + 4); // Allocate enough space
+                    packet_with_ident.extend_from_slice(packet); // Append the packet data
+                    let num_as_bytes = (num as u32 + 1).to_le_bytes(); // Convert num (usize) to 4 bytes (u32)
+                    packet_with_ident.extend_from_slice(&num_as_bytes); // Append the identifier
+                    udp_transmitter.send(&packet_with_ident).unwrap();
+                }
+                udp_transmitter.send(FRAME_END).unwrap();
             }
-            udp_transmitter.send(FRAME_END).unwrap();
         }
     });
 }
@@ -261,72 +273,16 @@ pub(crate) fn init_client_streams() {
 // Tests are very important when it comes to manipulating the frame
 #[cfg(test)]
 mod tests {
-    use std::fs::File;
-    use std::io::Read;
 
-    use openh264::decoder::Decoder;
+    use openh264::decoder::{Decoder, DecoderConfig};
     use v4l::video::Capture;
     use v4l::Device;
 
     use crate::stream::{FOURCC, HEIGHT, WIDTH};
 
-    use super::{CustomStream, FrameBuilder, H264Stream};
+    use super::{CustomStream, H264Stream, NalBuilder};
     const TEST_H264_FILE: &str = "test.h264";
 
-    #[test]
-    fn test_frame_initialization() {
-        let frame = FrameBuilder::new();
-        assert!(!frame.finished);
-        assert_eq!(frame.last_packet, 0);
-        assert_eq!(frame.buffers[0].len(), 1024 * 512);
-        assert!(frame.buffers[0].iter().all(|&byte| byte == 0));
-    }
-    #[test]
-    fn test_frame_end_detection() {
-        let mut frame = FrameBuilder::new();
-        let frame_end_buf = b"11111111111"; // Exact match with FRAME_END
-        frame.add_data(frame_end_buf, frame_end_buf.len());
-        assert!(frame.finished);
-    }
-    #[test]
-    fn test_add_data_to_frame() {
-        let mut frame = FrameBuilder::new();
-
-        // Simulating a packet with an identifier at the end (e.g., 0x00000001)
-        let packet_data: Vec<u8> = vec![0xAB; 500]; // Packet with 500 bytes of data
-        let ident: u32 = 1;
-        let ident_bytes = ident.to_le_bytes();
-        let mut buf = Vec::with_capacity(504);
-        buf.extend_from_slice(&packet_data);
-        buf.extend_from_slice(&ident_bytes); // Append the identifier
-
-        // Add data to frame
-        frame.add_data(&buf, buf.len());
-
-        // Check if data was added at the correct offset
-        assert_eq!(frame.last_packet, ident);
-        assert!(!frame.finished);
-
-        // Check that the received buffer has been updated
-        for (i, &byte) in packet_data.iter().enumerate() {
-            assert_eq!(frame.buffers[0][i], byte);
-        }
-    }
-
-    #[test]
-    fn test_frame_clear() {
-        let mut frame = FrameBuilder::new();
-
-        // Simulate that the frame has some data
-        frame.finished = true;
-        frame.last_packet = 10;
-        // Clear the frame
-        frame.switch_buffer();
-
-        // Check that all fields are reset
-        assert!(!frame.finished);
-        assert_eq!(frame.last_packet, 0);
-    }
     #[test]
     fn test_frame_encoding() {
         let device = Device::new(0).unwrap();
@@ -334,10 +290,10 @@ mod tests {
         device.set_format(&format).unwrap();
 
         let mut stream = H264Stream::new(&device);
-        let buf = stream.next_vec().unwrap();
+        let mut buf = Vec::with_capacity(WIDTH * HEIGHT);
+        stream.next(&mut buf);
 
         assert!(!buf.is_empty(), "Buffer is empty after encoding");
-
         assert!(
             buf.starts_with(&[0x00, 0x00, 0x00, 0x01]) || buf.starts_with(&[0x00, 0x00, 0x01]),
             "Encoded frame does not start with a valid H264 NAL unit start code"
@@ -345,15 +301,9 @@ mod tests {
     }
     #[test]
     fn test_frame_decoding() {
-        // Create and open the file
-        let mut file = File::open(TEST_H264_FILE).expect("Cannot open the test frame file");
-
-        // Create buffer and buffer reader
-        let mut buf: Vec<u8> = Vec::with_capacity(1024 * 100);
+        // encoded h264 stream
+        let bytes = include_bytes!("../test.h264");
         let mut decoder = Decoder::new().unwrap();
-
-        let size = file.read_to_end(&mut buf).unwrap();
-        assert!(size > 0);
 
         let mut frame_ref: Vec<u8> = Vec::new();
         let mut accumulated_data: Vec<u8> = Vec::new(); // Buffer to accumulate NAL units
@@ -363,7 +313,7 @@ mod tests {
         let mut pps_found = false;
 
         // Iterate over NAL units
-        for packet in openh264::nal_units(&buf[0..size]) {
+        for packet in openh264::nal_units(bytes) {
             let nal_type = packet[0] & 0x1F;
 
             // SPS NAL unit (NAL type 7)
